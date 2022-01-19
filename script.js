19fcff150562d97540ad2160ea9a732baa28c715
Beygs <boris.gilles@hotmail.fr>
2022-01-19 20:47:25 +0100

    Basic use done

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 import { ENV } from "./env.js";
-
+var movies = document.querySelector(".movies");
+var form = document.querySelector("form");
+var searchMovie = function (search, page) {
+    if (page === void 0) { page = 1; }
+    var url = "http://www.omdbapi.com/?apikey=".concat(ENV["OMDB_KEY"], "&s=").concat(search, "&p=").concat(page);
+    console.log(url);
+    fetch(url)
+        .then(function (response) { return response.json(); })
+        .then(function (result) { return result.Search.forEach(function (movie) { return movieDetails(movie.Title); }); })["catch"](function (error) { return console.error("Oups ! Une erreur a été rencontrée =>" + error); });
+};
+var movieDetails = function (movieTitle) {
+    var url = "http://www.omdbapi.com/?apikey=".concat(ENV["OMDB_KEY"], "&t=").concat(movieTitle);
+    fetch(url)
+        .then(function (response) { return response.json(); })
+        .then(function (result) { return showMovie(result); })["catch"](function (error) { return console.error("Oups ! Une erreur a été rencontrée =>" + error); });
+};
+var showMovie = function (movieInfos) {
+    var Poster = movieInfos.Poster, Title = movieInfos.Title, Type = movieInfos.Type, Year = movieInfos.Year;
+    movies.innerHTML += "\n    <div class=\"movie\">\n      <img src=\"".concat(Poster, "\" alt=\"Movie Poster\" class=\"movie__poster\">\n      <h3 class=\"movie__title\">").concat(Title, "</h3>\n      <h4 class=\"movie__type\">").concat(Type, "</h4>\n      <h4 class=\"movie__year>").concat(Year, "</h4>\n    </div>\n  ");
+};
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var input = form.querySelector("#movieSearch");
+    movies.innerHTML = "";
+    searchMovie(input.value);
+});
--- script.ts
+++ script.ts
@@ -1,3 +1,66 @@
-import ENV from "./env.js";
+import { ENV } from "./env.js";
 
-console.log(ENV);
+const movies = document.querySelector(".movies");
+const form = document.querySelector("form");
+
+interface Movie {
+  Actors: string;
+  Awards: string;
+  BoxOffice: string;
+  Country: string;
+  Director: string;
+  Genre: string;
+  Language: string;
+  Plot: string;
+  Poster: string;
+  Production: string;
+  Rated: string;
+  Ratings: { Source: string; Value: string; }[];
+  Released: string;
+  Title: string;
+  Type: string;
+  Writer: string;
+  Year: string;
+  [key: string]: any;
+}
+
+const searchMovie = (search: string, page = 1): void => {
+  const url = `http://www.omdbapi.com/?apikey=${ENV["OMDB_KEY"]}&s=${search}&p=${page}`;
+  console.log(url);
+
+  fetch(url)
+    .then(response => response.json())
+    .then(result => result.Search.forEach(movie => movieDetails(movie.Title)))
+    .catch(error => console.error("Oups ! Une erreur a été rencontrée =>" + error));
+}
+
+const movieDetails = (movieTitle: string): void => {
+  const url = `http://www.omdbapi.com/?apikey=${ENV["OMDB_KEY"]}&t=${movieTitle}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(result => showMovie(result))
+    .catch(error => console.error("Oups ! Une erreur a été rencontrée =>" + error));
+}
+
+const showMovie = (movieInfos: Movie): void => {
+  const { Poster, Title, Type, Year } = movieInfos;
+
+  movies.innerHTML += `
+    <div class="movie">
+      <img src="${Poster}" alt="Movie Poster" class="movie__poster">
+      <h3 class="movie__title">${Title}</h3>
+      <h4 class="movie__type">${Type}</h4>
+      <h4 class="movie__year>${Year}</h4>
+    </div>
+  `
+}
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const input: HTMLInputElement = form.querySelector("#movieSearch");
+  movies.innerHTML = "";
+
+  searchMovie(input.value);
+})
